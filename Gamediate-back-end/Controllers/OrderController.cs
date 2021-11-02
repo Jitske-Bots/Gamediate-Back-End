52fe7b5914c13c79c6a0f7328306838a3e5c80a4
Jitske-Bots <58811242+Jitske-Bots@users.noreply.github.com>
2021-11-02 15:17:11 +0100

    added cookies for the shoppingcart

--- Gamediate-back-end/Controllers/OrderController.cs
+++ Gamediate-back-end/Controllers/OrderController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Gamediate_back_end.BLL;
 using Gamediate_back_end.Models;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace Gamediate_back_end.Controllers
 {
@@ -20,18 +22,44 @@ namespace Gamediate_back_end.Controllers
         }
 
         /// <summary>
-        /// this method will get the list of orderitems
-        /// add an order
-        /// and add orderitems
+        /// this method will get the gameID and the price from the added game
+        /// get the list
+        /// and add a dictionary to the list
         /// </summary>
         /// <param name="order"></param>
         /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> AddOrder([FromBody]Order order)
+        public async Task<IActionResult> AddOrderItem([FromBody]int gameID, int price)
         {
-            order.OrderDate = DateTime.Now;
-            orderBLL.Add(order);
+            List<Dictionary<string, int>> shoppingCart = this.GetShoppingCart();
+            shoppingCart.Add(
+            new Dictionary<string, int>() {
+                    { "gameID", gameID },
+                    { "price", price },
+            });
+
+            //create new cookie
+            //cookies are saved for 1 hour
+            //append the shoppingcart to the cookie
+            CookieOptions option = new CookieOptions();
+            option.Expires = DateTime.Now.AddHours(1);
+            Response.Cookies.Append("shoppingCart", JsonSerializer.Serialize(shoppingCart), option);
+
             return Ok();
+        }
+
+        private List<Dictionary<string, int>> GetShoppingCart()
+        {
+            //Dictionary is a generic collection which is generally used to store key/value pairs.
+            string value = Request.Cookies["shoppingCart"];
+            if (value != null)
+            {
+                return JsonSerializer.Deserialize<List<Dictionary<string, int>>>(value);
+            }
+            else
+            {
+                return new List<Dictionary<string, int>>();
+            }
         }
     }
 }
