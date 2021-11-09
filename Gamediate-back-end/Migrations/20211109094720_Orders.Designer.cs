688c7d76cb0b5b8d5b3d379f9d59f4131dd465cd
Jitske-Bots <58811242+Jitske-Bots@users.noreply.github.com>
2021-11-09 13:52:50 +0100

    added migrations for order and orderItem

--- Gamediate-back-end/Migrations/20211109094720_Orders.Designer.cs
+++ Gamediate-back-end/Migrations/20211109094720_Orders.Designer.cs
@@ -4,14 +4,16 @@ using Gamediate_back_end.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Gamediate_back_end.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20211109094720_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
